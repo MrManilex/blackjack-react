a84964e1b61927d0bb05e643451b4589107c7036
Alexander <alexandermaniwang99@gmail.com>
2023-11-25 16:09:05 -0500

    install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,12 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    "node_modules/preline/dist/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("preline/plugin")
+    require("daisyui")
   ],
 }
 
