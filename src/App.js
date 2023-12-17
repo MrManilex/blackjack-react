710d3ba2a5e2981ff2b7bb8502798ab675287753
Alexander <alexandermaniwang99@gmail.com>
2023-12-17 16:02:24 -0500

    create handle start game function and conditionally render the board.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import Board from './components/Board'
 import { useState } from 'react'
 
 function App() {
-  // const [game, finished] = useState(false)
+  const [game, start] = useState(false)
   const [winner, isWinner] = useState(false)
+
+  const handleStartGame = () => {
+    // logic to start the game
+    start(true)
+  }
 
 
   return (
     <>
-      <main className='min-h-screen'>
+      <main className='min-h-screen bg-green-700'>
         <div className='board flex justify-center w-full'>
           <div>
-            <Board
-              // game={game}
-              // finished={finished}
-              winner={winner}
-              isWinner={isWinner}
-            />
+            {game ?
+              <>
+                <Board
+                  // game={game}
+                  // finished={finished}
+                  winner={winner}
+                  isWinner={isWinner}
+                />
+              </>
+              :
+              <>
+                <button className='btn btn-primary' onClick={handleStartGame}>
+                  Start Game
+                </button>
+              </>
+            }
           </div>
         </div>
       </main>
